a1bb9fda42bdb95ff44072e700d10933c521b6b2
Chengyu Song <csong@cs.ucr.edu>
2020-07-24 12:53:45 -0700

    temporarily disable libcxx before compatibility issue is resolved

--- llvm_mode/compiler/ko_clang.c
+++ llvm_mode/compiler/ko_clang.c
@@ -265,7 +265,7 @@ static void edit_params(u32 argc, char **argv) {
     //cc_params[cc_par_cnt++] = "-funroll-loops";
   }
 
-  if (is_cxx) {
+  if (0 && is_cxx) {
     // FIXME: or use the same header
     // cc_params[cc_par_cnt++] = "-I/path-to-llvm/include/c++/v1";
     cc_params[cc_par_cnt++] = alloc_printf("-L%s/lib/libcxx_track/", obj_path);
