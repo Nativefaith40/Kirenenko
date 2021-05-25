26008e2bf4e7e4d0a6dd0f0cfcb2141c4366bd76
Chengyu Song <csong@cs.ucr.edu>
2021-05-25 15:45:06 -0700

    update hashtable impl, use xxhash, try to avoid data races with atomic operations

--- llvm_mode/dfsan_rt/dfsan/union_hashtable.cc
+++ llvm_mode/dfsan_rt/dfsan/union_hashtable.cc
@@ -1,4 +1,4 @@
-#include "../sanitizer_common/sanitizer_libc.h"
+#include "sanitizer_common/sanitizer_libc.h"
 #include "union_hashtable.h"
 #include "union_util.h"
 
@@ -6,36 +6,41 @@ using namespace __taint;
 
 union_hashtable::union_hashtable(uint64_t n) {
   bucket_size = n;
-  bucket = (struct union_hashtable_entry **)
-    allocator_alloc(n * sizeof(struct union_hashtable_entry *));
-  __sanitizer::internal_memset(bucket, 0, n * sizeof(struct union_hashtable_entry *));
+  bucket = reinterpret_cast<atomic_uintptr_t*>(
+      allocator_alloc(n * sizeof(atomic_uintptr_t)));
+  __sanitizer::internal_memset(bucket, 0, n * sizeof(atomic_uintptr_t));
 }
 
-uint64_t
+uint32_t
 union_hashtable::hash(const dfsan_label_info &key) {
-  return ((key.l1 + key.l2 + key.op1 + key.op2) ^ (key.op + key.size)) 
-      & (bucket_size - 1);
+  return key.hash & (bucket_size - 1);
 }
 
 void
 union_hashtable::insert(dfsan_label_info *key, dfsan_label entry) {
-  uint64_t index = hash(*key);
+  uint32_t index = hash(*key);
   auto curr = (struct union_hashtable_entry *)
       allocator_alloc(sizeof(struct union_hashtable_entry));
   curr->key = key; curr->entry = entry;
-  curr->next = bucket[index];
-  bucket[index] = curr;
+  uptr p = atomic_load(&bucket[index], memory_order_acquire);
+  while (true) {
+    curr->next = reinterpret_cast<struct union_hashtable_entry *>(p);
+    if (atomic_compare_exchange_strong(&bucket[index], &p, (uptr)curr,
+                                       memory_order_seq_cst))
+      break; // spin until succeed, when fail, p will contain the current head
+  }
 }
 
 option
 union_hashtable::lookup(const dfsan_label_info &key) {
   uint64_t index = hash(key);
-  auto curr = bucket[index];
+  uptr p = atomic_load(&bucket[index], memory_order_acquire);
+  auto curr = reinterpret_cast<struct union_hashtable_entry *>(p);
   while (curr) {
     if (*(curr->key) == key) {
       return some_dfsan_label(curr->entry);
     }
-    curr = curr->next;
+    curr = curr->next; // no data race here
   }
   return none();
 }
--- llvm_mode/dfsan_rt/dfsan/union_hashtable.h
+++ llvm_mode/dfsan_rt/dfsan/union_hashtable.h
@@ -2,10 +2,17 @@
 #define UNION_HASHTABLE_H
 
 #include <stdint.h>
+#include "sanitizer_common/sanitizer_atomic.h"
 #include "sanitizer_common/sanitizer_internal_defs.h"
 #include "taint_allocator.h"
 #include "union_util.h"
 #include "dfsan.h"
+
+using __sanitizer::atomic_uintptr_t;
+using __sanitizer::atomic_load;
+using __sanitizer::atomic_compare_exchange_strong;
+using __sanitizer::memory_order_acquire;
+using __sanitizer::memory_order_seq_cst;
 
 namespace __taint {
 
@@ -16,9 +23,9 @@ struct union_hashtable_entry {
 };
 
 class union_hashtable {
-  struct union_hashtable_entry **bucket;
+  atomic_uintptr_t *bucket;
   uint64_t bucket_size;
-  uint64_t hash(const dfsan_label_info &key);
+  uint32_t hash(const dfsan_label_info &key);
 public:
   union_hashtable(uint64_t n);
   void insert(dfsan_label_info *key, dfsan_label value);
